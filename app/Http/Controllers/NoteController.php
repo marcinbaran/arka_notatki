ae833dd1ed4dae8d739013e9255b5768bfb01a2f
Marcin <mrevib@gmail.com>
2025-01-07 17:52:05 +0100

    Uzupełniłem funkcje w kontrolerze do obsługi notatek

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Note;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class NoteController extends Controller
@@ -10,25 +12,34 @@ class NoteController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
-        //
+        $notes = Note::all();
+
+        return view('notes.index', compact('notes'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('notes.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required',
+        ]);
+
+        Note::create($request->all());
+
+        return redirect()->route('notes.index')->with('success', 'Utworzono notatkę !');
     }
 
     /**
@@ -42,24 +53,33 @@ class NoteController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Note $note)
+    public function edit(Note $note): View
     {
-        //
+        return view('notes.edit', compact('note'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Note $note)
+    public function update(Request $request, Note $note): RedirectResponse
     {
-        //
+        $request->validate([
+            'title' => 'required|max:255',
+            'content' => 'required',
+        ]);
+
+        $note->update($request->all());
+
+        return redirect()->route('notes.index')->with('success', 'Notatka została edytowana !');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Note $note)
+    public function destroy(Note $note): RedirectResponse
     {
-        //
+        $note->delete();
+
+        return redirect()->route('notes.index')->with('success', 'Notatka została usunięta !');
     }
 }
