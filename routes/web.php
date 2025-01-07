d668af8a23abfdb30ba0a14b41b996435963841c
Marcin <mrevib@gmail.com>
2025-01-07 17:57:50 +0100

    Dodałem router do notatek

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/notes');
 });
+
+Route::resource('notes', NoteController::class);
